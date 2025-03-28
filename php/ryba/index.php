1ca64dd2a5f6f3e40ff40b386de2eb257e862f55
vlabok <vladgithub8@gmail.com>
2025-03-28 14:39:30 +0100

    sd

---
+++ php/ryba/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Portal dla wędkarzy</h1>
+    </div>
+
+    <div class="container">
+        <section class="left-block">
+            <h3>Ryby zamieszkujące rzeki</h3>
+            <ol>
+                <li>Szczupak pływa w rzece Warta-Obrzycko, Wielkopolskie</li>
+                <li>Leszcz pływa w rzece Przemsza k. Okradzionowa, Śląskie</li>
+            </ol>
+        </section>
+
+        <section class="left-block">
+            <h3>Ryby drapieżne naszych wód</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>L.p.</th>
+                        <th>Gatunek</th>
+                        <th>Występowanie</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Szczupak</td>
+                        <td>stawy, rzeki</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sandacz</td>
+                        <td>stawy, jeziora, rzeki</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Okoń</td>
+                        <td>rzeki</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Sum</td>
+                        <td>jeziora, rzeki</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Dorsz</td>
+                        <td>morza, oceany</td>
+                    </tr>
+                </tbody>
+            </table>
+            <a href="kwerendy.txt" class="download-link">Pobierz kwerendy</a>
+        </section>
+
+        <section class="right-block">
+            <img src="ryba1.jpg" alt="Sum">
+        </section>
+    </div>
+
+    <div class="footer">
+        <p>Stronę wykonał: 00000000000</p>
+    </div>
+</body>
+</html>

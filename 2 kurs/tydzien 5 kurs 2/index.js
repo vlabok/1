c1b7d4b67f7533edb1414af91616819ae80c5382
vlabok <vladgithub8@gmail.com>
2025-10-24 12:09:51 +0300

    89

---
+++ 2 kurs/tydzien 5 kurs 2/index.js
@@ -0,0 +1,15 @@
+const zdanie = "Złota polska jesień!";
+
+
+let i = 0;
+ while (i < zdanie.length){
+console.log(zdanie[i]);
+    i++;
+ };
+
+
+/*
+for (let i = 0; i < zdanie.length; i++) {
+    console.log(zdanie[i]);
+}
+ */

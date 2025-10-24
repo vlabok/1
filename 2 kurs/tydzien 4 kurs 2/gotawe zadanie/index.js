314f336b0dd862fbfcff7597d26e02967ba13e62
vlabok <vladgithub8@gmail.com>
2025-10-24 11:50:44 +0300

    Update index.js

--- 2 kurs/tydzien 4 kurs 2/gotawe zadanie/index.js
+++ 2 kurs/tydzien 4 kurs 2/gotawe zadanie/index.js
@@ -2,22 +2,26 @@ const button = document.getElementById("sprawdz");
 const input = document.getElementById("inputt");
 
 button.addEventListener("click", function () {
-  const haslo = input.value.trim();
+  const haslo = input.value;
+
+  const maCyfre = /\d/.test(haslo);
+  const maSpecjalny = /[!@#$%^&*(),.?":{}|<>]/.test(haslo);
+  const maDuzaLitere = /[A-Z]/.test(haslo);
 
   if (haslo === "") {
     wynik.textContent = "WPISZ HASŁO!";
     wynik.style.color = "red";
-  } else if (haslo.length < 5) {
-    wynik.textContent = "SŁABE";
-    wynik.style.color = "orange";
-  } else if (haslo.length < 10) {
-    wynik.textContent = "ŚREDNIE";
-    wynik.style.color = "blue";
-  } else if (haslo.length < 18) {
-    wynik.textContent = "DOBRE";
-    wynik.style.color = "greenyellow";
-  } else {
-    wynik.textContent = "BARDZO DOBRE";
+  } else if (haslo.length >= 18 && maCyfre && maSpecjalny && maDuzaLitere) {
+    wynik.textContent = "BARDZO DOBRY";
     wynik.style.color = "green";
+  } else if (haslo.length >= 10 && haslo.length <= 17 && maCyfre) {
+    wynik.textContent = "DOBRY";
+    wynik.style.color = "greenyellow";
+  } else if (haslo.length >= 5 && haslo.length <= 9) {
+    wynik.textContent = "ŚREDNI";
+    wynik.style.color = "blue";
+  } else {
+    wynik.textContent = "SŁABY";
+    wynik.style.color = "orange";
   }
 });
